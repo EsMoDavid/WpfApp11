2993765e8d382c4eb473a3946ae9c68fd80b3596
薛春翔 <showyoumymercy@live.com>
2020-03-30 14:47:36 +0800

    add ref calss

--- WpfApp11/MainWindow.xaml.cs
+++ WpfApp11/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,7 @@ namespace WpfApp11
         {
             InitializeComponent();
             //string a = "1;0001490889
+            tb.Text = JsonConvert.SerializeObject(new object());
         }   
     }
 }
