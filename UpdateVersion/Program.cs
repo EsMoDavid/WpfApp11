ccff86e8d868cd86a6f5ed9a501846b953874a56
薛春翔 <showyoumymercy@live.com>
2020-03-31 17:02:10 +0800

    update

--- UpdateVersion/Program.cs
+++ UpdateVersion/Program.cs
@@ -3,26 +3,30 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UpdateVersion
 {
     class Program
     {
+        const string KeyAssembly = "AssemblyVersion(\"";
         static void Main(string[] args)
         {
-            string previous = args[0];
-            string current = args[1];
-            Console.WriteLine(previous);
-            Console.WriteLine(current);
-            string previousVer = $"[assembly: AssemblyVersion(\"{previous}\")]";
-            string currentVer = $"[assembly: AssemblyVersion(\"{current}\")]";
+            List<string> currentVer = args[0].Split('.').ToList();
+            int nextVerIndex = int.Parse(currentVer.Last()) + 1;
+            currentVer[currentVer.Count - 1] = nextVerIndex.ToString();
+            string newVer = string.Join(".", currentVer);
             string path= @"WpfApp11\Properties\AssemblyInfo.cs";
             if (File.Exists(path))
             {
                 string assebmlyVer = File.ReadAllText(path);
-                var newAssemblyInfo = assebmlyVer.Replace(previousVer, currentVer);
-                File.WriteAllText(path, newAssemblyInfo);
+                int startIndex = assebmlyVer.LastIndexOf(KeyAssembly);
+                int len = KeyAssembly.Length;
+                int endIndex = assebmlyVer.IndexOf("\"", startIndex + len);
+                string oldVer = assebmlyVer.Substring(startIndex + len, endIndex - startIndex - len);
+                assebmlyVer = assebmlyVer.Replace(oldVer, newVer);
+                File.WriteAllText(path, assebmlyVer);
             }
         }
     }
