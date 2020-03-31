f61b35820dd30e0cda00725db370d35e371db98a
薛春翔 <showyoumymercy@live.com>
2020-03-31 17:15:31 +0800

    update_ver

--- WpfApp11/MainWindow.xaml.cs
+++ WpfApp11/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace WpfApp11
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window 
-    {
+    { 
         public MainWindow()
         {
             InitializeComponent();
