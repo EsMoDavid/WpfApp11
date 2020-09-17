bed71b796a932f2f82f2f34dd5bcdbd69d1e02cf
薛春翔 <showyoumymercy@live.com>
2020-09-17 17:34:22 +0800

    update_ver

--- WpfApp11/Properties/AssemblyInfo.cs
+++ WpfApp11/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.72")]
+[assembly: AssemblyVersion("1.0.68")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
